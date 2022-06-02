579081b29d2ca4625d493bba72c2477eab14115d
ashutoshd1996 <36852252+ashutoshd1996@users.noreply.github.com>
2022-06-02 07:55:44 +0200

    Adding Data folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applied Data Science on covid-19 data',
+    author='Ashutosh Dinesh',
+    license='',
+)
